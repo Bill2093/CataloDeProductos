1f7aeb6b209f2d9cb01f833ee06569de521a660d
Bill2093 <pedroluisoa20@gmail.com>
2025-08-04 11:32:43 -0500

    Creacion de Clases, en Data y en productos

---
+++ CataloDeProductos/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CataloDeProductos.Models;
+
+namespace CataloDeProductos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
